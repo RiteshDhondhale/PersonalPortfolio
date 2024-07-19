ed73ffeeeafd5cfd0b19544a78a994e802480b31
RiteshDhondhale <163043089+RiteshDhondhale@users.noreply.github.com>
2024-07-19 20:10:33 +0530

    Update the Nav Link CSS Customization

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const navLinkEls = document.querySelectorAll('.nav-link');
-const sectionEls = document.querySelectorAll('.section');
+// const navLinkEls = document.querySelectorAll('.nav-link');
+// const sectionEls = document.querySelectorAll('.section');
 
 
-//Nav 
-let currentSection = 'about';
-window.addEventListener('scroll', () => {
-    sectionEls.forEach(sectionEl => {
-        if(window.scrollY >= sectionEl.offsetTop){
-            currentSection = sectionEl.id;
-        }
-    })
+// //Nav 
+// let currentSection = 'about';
+// window.addEventListener('scroll', () => {
+//     sectionEls.forEach(sectionEl => {
+//         if(window.scrollY >= sectionEl.offsetTop){
+//             currentSection = sectionEl.id;
+//         }
+//     })
 
-    navLinkEls.forEach(navLinkEL => {
-        if(navLinkEL.href.includes(currentSection)){
-            navLinkEL.classList.add('active');
-            navLinkEL.childNodes[1].style.width = "50px";
-            // navLinkEL.childNodes[1].style.backgroundColor = "rgb(226 232 240)";
-        }
-        if(navLinkEL.href.includes(!currentSection)){
-            navLinkEL.classList.remove('active');
-            navLinkEL.childNodes[1].style.width = "2rem";
-            // navLinkEL.childNodes[1].style.backgroundColor = "rgb(226 232 240)";
-        }
-    })
-});
+//     navLinkEls.forEach(navLinkEL => {
+//         if(navLinkEL.href.includes(currentSection)){
+//             navLinkEL.classList.add('active');
+//             navLinkEL.childNodes[1].style.width = "50px";
+//             // navLinkEL.childNodes[1].style.backgroundColor = "rgb(226 232 240)";
+//         }
+//         if(navLinkEL.href.includes(!currentSection)){
+//             navLinkEL.classList.remove('active');
+//             navLinkEL.childNodes[1].style.width = "2rem";
+//             // navLinkEL.childNodes[1].style.backgroundColor = "rgb(226 232 240)";
+//         }
+//     })
+// });
 
 //Cursor Animation
 const cursor = document.querySelector(".cursor");
